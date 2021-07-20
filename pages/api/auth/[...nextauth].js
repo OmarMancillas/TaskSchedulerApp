6f243ec86784ba13a8ba4d7852de154f6f02780a
Omar <43597862+OmarMancillas@users.noreply.github.com>
2021-07-19 21:12:36 -0500

    Fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,16 +5,16 @@ export default NextAuth({
   providers: [
     // OAuth authentication providers...
     Providers.GitHub({
-      // clientId: process.env.GITHUB_CLIENT_ID,
-      // clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      clientId : "7225219cd90aaf6bb561",
-      clientSecret: "67902972aa342ec01f83cb99ca3acc78c49c6c75",
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      // clientId : "7225219cd90aaf6bb561",
+      // clientSecret: "67902972aa342ec01f83cb99ca3acc78c49c6c75",
     }),
     Providers.Google({
-      // clientId: process.env.GOOGLE_CLIENT_ID,
-      // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      clientId: "754978520057-rnoi2f0vdq5asgupht681567d7npm7rh.apps.googleusercontent.com",
-      clientSecret: "RvQKFBsw18cXzK3BAKzAumJW",
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      // clientId: "754978520057-rnoi2f0vdq5asgupht681567d7npm7rh.apps.googleusercontent.com",
+      // clientSecret: "RvQKFBsw18cXzK3BAKzAumJW",
     }),
   ],
 });
